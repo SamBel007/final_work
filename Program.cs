f4843d7e88a8cbbbd086a7f9c9fa5acff06278bc
Alex Beloglazov <barber004@mail.ru>
2022-09-06 00:09:44 +0300

    2punkt

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿string[] M;
 Console.WriteLine("Введите массив строк через пробел");
 string stroka = Console.ReadLine();
+M = stroka.Split(' ');
+var result = new string[M.Length];
+var realSize = 0;
+foreach (var value in M)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
